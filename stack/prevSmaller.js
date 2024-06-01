8b9b1d86d620be8ed59953302a618319e2c064df
Ashish Kumar <ashish1294952@gmail.com>
2024-06-01 16:13:17 +0530

    added prev smaller

---
+++ stack/prevSmaller.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+
+function top(st) {
+    return st.length && st[st.length-1]
+}
+var previousSmaller = function(nums) {
+    let n = nums.length
+    if(nums.length == 0)
+    return []
+    let st =[]
+    st.push(n-1)
+    let res = []
+    for (let i=n-2; i>=0 ;i--) {
+        while(st.length && nums[top(st)] > nums[i])
+        {
+            res[st.pop()]= nums[i]
+        }
+ 
+            st.push(i)
+    }
+
+    while(st.length) {
+        let val = st.pop()
+        res[val] =  -1 
+    }
+
+    return res
+};
+
+
+
+let  nums2 = [1, 3, 0, 2, 5]
+console.log(previousSmaller(nums2))
