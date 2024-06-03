185eed4aa7c52c633c2647f4752f256ad931a920
Ashish Kumar <ashish1294952@gmail.com>
2024-06-03 21:09:13 +0530

    sort and reversing the stack

--- stack/stack.js
+++ stack/stack.js
@@ -25,6 +25,50 @@ class Stack {
     size() {
         return this.items.length;
     }
+
+    print() {
+        let stackString = "";
+        for (let i = 0; i < this.items.length; i++) {
+            stackString += this.items[i] + " ";
+        }
+        console.log(stackString.trim());
+    }
+
+    insert_at_bottom(x) {
+        if(this.isEmpty()) {
+            this.push(x);
+        } else {
+            let a = this.pop();
+            this.insert_at_bottom(x);
+            this.push(a);
+        }
+    }
+    insert_at_sorted_position(x) {
+        if(this.isEmpty() || this.peek() > x) {
+            this.push(x);
+        } else {
+            let a = this.pop();
+            this.insert_at_sorted_position(x);
+            this.push(a);
+        }
+    }
+    reverse() {
+        if(!this.isEmpty()) {
+
+            let a  = this.pop();
+            this.reverse()
+            this.insert_at_bottom(a)
+        } 
+    }
+
+    sort() {
+        if(!this.isEmpty()) {
+
+            let a  = this.pop();
+            this.sort()
+            this.insert_at_sorted_position(a)
+        }
+    }
 }
 
 // Example of using a stack to simulate recursion for factorial calculation
@@ -33,5 +77,18 @@ module.exports = Stack
 // Example usage:
 
 if (require.main === module) {
-console.log("Factorial of 5 using recursion stack:", factorialRecursionStack(5)); // Output: 120
+//console.log("Factorial of 5 using recursion stack:", factorialRecursionStack(5)); // Output: 120
+let stack = new Stack();
+stack.push(4);
+stack.push(5);
+stack.push(2);
+stack.push(1);
+stack.push(3);
+stack.print();
+stack.reverse();
+console.log("After reversing")
+stack.print();
+stack.sort();
+console.log("After sorting")
+stack.print();
 }
