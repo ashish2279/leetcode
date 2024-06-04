9a78dd69e76734c29f0f725102b27ee33907b738
Ashish Kumar <ashish1294952@gmail.com>
2024-06-05 00:20:27 +0530

    Added sorting algo

---
+++ Algo/bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i =0;i<n-1;i++)
+        for (let j = 0; j<n - i - 1 ; j++) {
+                if (arr[j] > arr[j + 1]) {
+                
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+        return arr;
+}
+
+
+let arr = [3,5,1,0,-1]
+
+console.log(bubbleSort(arr))
---
+++ Algo/selectionSort.js
@@ -0,0 +1,16 @@
+function Sort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+    return arr;
+}
+
+let arr2 = [3,8,7,6,9,0,-0];
+console.log( Sort(arr2)); 
