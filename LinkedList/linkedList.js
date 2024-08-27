abe6bc61648b38dbbc03c0545107413104de3491
Ashish Kumar <ashish1294952@gmail.com>
2024-08-28 00:41:22 +0530

    sort a linked list

--- LinkedList/linkedList.js
+++ LinkedList/linkedList.js
@@ -1,4 +1,4 @@
-class Node{
+class Node {
     constructor(val) {
         this.val = val;
         this.next = null
@@ -12,170 +12,101 @@ class LinkedList {
         this.count = 0;
     }
 
-    peek() 
-    {
-        if(this.head){
-
-            console.log("peek element " + this.head.val)
-            return this.head.val
-        }
-        else
-         console.log("Empty list ")
-    }
-    insert(val) {
-        this.count++;
-        if(!this.head) {
-            this.head = new Node(val)
-            console.log(val + " inserted")
-            return ;
-        }
-
-        let curr = this.head;
-        let  prev;
-        while (curr) {
-            prev = curr
-            curr = curr.next
-        }
-        let n = new Node(val)
-         prev.next = n;
-         console.log(val + " inserted")
-         
-         return n;
-            
-    }
-    insertHead(val) {
-        this.count++;
-        if(!this.head) {
-            this.head = new Node(val)
-            console.log(val + " inserted")
-            return ;
-        }
-
-        let head = this.head;
-        let n = new Node(val)
-         n.next = head;
-         console.log(val + " inserted at head")
-         this.head = n;
-         return n;
-            
-    }
-    print () {
-        if(!this.head) {
-            console.log("Empty List");
-            return ;
-        }
-        let curr = this.head;
-        let res= "";
-        while(curr) {
-            if (Number(curr.val) < 0)
-                 res += "(" + curr.val + ") --> "
-            else
-                res +=  curr.val + " --> "
-            curr = curr.next
-        }
-        res = res.slice(0, res.length-5);
-        console.log(res)
-    }
-
-    deleteLinkedList() {
-        console.log("deleting the complete list");
-        if(!this.head) {
-            console.log("Empty List");
+    delete(val) {
+        if (!this.head) {
+            console.log("Empty List")
             return;
         }
-        let head = this.head;
-        while(head) {
-            this.deletelast();
-            head = this.head;
-        }
-    }
-    delete(val) {
-        if(!this.head) {
-            console.log("Empty List")
-            return ;
-        }
 
         let curr = this.head;
-        let  prev;
+        let prev;
         while (curr && curr.val != val) {
             prev = curr
             curr = curr.next
         }
-        if(!curr)
-            {
-                console.log("Node not found")
-                return;
-            }
+        if (!curr) {
+            console.log("Node not found")
+            return;
+        }
         else {
             console.log(curr.val + " deleted")
             this.count--;
-            if(curr == this.head)
-                {
-                    this.head = curr.next
-                }
-            else
-            {
+            if (curr == this.head) {
+                this.head = curr.next
+            }
+            else {
 
                 prev.next = curr.next
-          }
+            }
             return;
         }
-         
-         
+
+
     }
 
-    deletelast() {
-        if(!this.head) {
+    deletefirst() {
+        if (!this.head) {
             console.log("Empty List")
-            return ;
+            return;
         }
 
         let curr = this.head;
-        let  prev;
+        let next = curr.next;
+        curr.next = null;
+        this.head = next;
+        this.count--;
+    }
+
+    deletelast() {
+        if (!this.head) {
+            console.log("Empty List")
+            return;
+        }
+
+        let curr = this.head;
+        let prev;
         while (curr && curr.next) {
             prev = curr
             curr = curr.next
         }
 
-        if(!curr)
-            {
-                console.log("Empty list")
-                return;
-            }
+        if (!curr) {
+            console.log("Empty list")
+            return;
+        }
         else {
             console.log(curr.val + " deleted")
             this.count--;
-            if(curr == this.head)
-                {
-                    this.head = curr.next
-                }
-            else
-            {
+            if (curr == this.head) {
+                this.head = curr.next
+            }
+            else {
 
                 prev.next = curr.next
-0            }
+                0
+            }
             return;
         }
-                  
+
     }
 
-    deletefirst() {
-        if(!this.head) {
-            console.log("Empty List")
-            return ;
+    deleteLinkedList() {
+        console.log("deleting the complete list");
+        if (!this.head) {
+            console.log("Empty List");
+            return;
         }
-
-        let curr = this.head;
-        let next  = curr.next;
-        curr.next = null;
-        this.head = next;
-        this.count--;          
+        let head = this.head;
+        while (head) {
+            this.deletelast();
+            head = this.head;
+        }
     }
 
     detectCycle() {
         let head = this.head;
-        if(!head)
-            return {val : 'Not present'};
+        if (!head)
+            return { val: 'Not present' };
 
         let slow = head;
         let fast = head;
@@ -183,9 +114,9 @@ class LinkedList {
         while (slow && fast && fast.next) {
             slow = slow.next;
             fast = fast.next.next;
-            if(fast == slow){
+            if (fast == slow) {
                 let start = head;
-                while(start!==slow) {
+                while (start !== slow) {
                     start = start.next;
                     slow = slow.next;
                 }
@@ -193,43 +124,236 @@ class LinkedList {
             }
         }
 
-        return {val : 'Not present'};
+        return { val: 'Not present' };
 
     }
 
+    insert(val) {
+        this.count++;
+        if (!this.head) {
+            this.head = new Node(val)
+            // console.log(val + " inserted at head")
+            return;
+        }
+
+        let curr = this.head;
+        let prev;
+        while (curr) {
+            prev = curr
+            curr = curr.next
+        }
+        let n = new Node(val)
+        prev.next = n;
+        // console.log(val + " inserted")
+
+        return n;
+
+    }
+
+    insertHead(val) {
+        this.count++;
+        if (!this.head) {
+            this.head = new Node(val)
+            console.log(val + " inserted")
+            return;
+        }
+
+        let head = this.head;
+        let n = new Node(val)
+        n.next = head;
+        console.log(val + " inserted at head")
+        this.head = n;
+        return n;
+
+    }
+    insertAtSortedPosition(val) {
+        this.count++;
+        if (!this.head) {
+            this.head = new Node(val)
+            console.log(val + " inserted")
+            return;
+        }
+
+        let curr = this.head;
+        let prev;
+        while (curr && curr.val < val) {
+            prev = curr
+            curr = curr.next;
+        }
+        let next = prev ? prev.next : null;
+        let n = new Node(val)
+        if (prev) {
+            let next = prev.next;
+            prev.next = n;
+            n.next = next;
+            console.log(val + " inserted at right position")
+    
+            return n;
+        }else {
+            return this.insertHead(val);
+        }
+    
+
+    }
+    peek() {
+        if (this.head) {
+
+            console.log("peek element " + this.head.val)
+            return this.head.val
+        }
+        else
+            console.log("Empty list ")
+    }
+
+    print() {
+        if (!this.head) {
+            console.log("Empty List");
+            return;
+        }
+        let curr = this.head;
+        let res = "";
+        while (curr) {
+            if (Number(curr.val) < 0)
+                res += "(" + curr.val + ") --> "
+            else
+                res += curr.val + " --> "
+            curr = curr.next
+        }
+        res = res.slice(0, res.length - 5);
+        console.log(res)
+    }
+
+    printL(head) {
+        if (!head) {
+            console.log("Empty List");
+            return;
+        }
+        let curr = head;
+        let res = "";
+        while (curr) {
+            if (Number(curr.val) < 0)
+                res += "(" + curr.val + ") --> "
+            else
+                res += curr.val + " --> "
+            curr = curr.next
+        }
+        res = res.slice(0, res.length - 5);
+        console.log(res)
+    }
     size() {
         return this.count;
     }
+
+    findtwoHalves(head) {
+        let slow = head;
+        let fast = slow;
+        if(!slow) {
+            return {firstHead: head, secondHead: head };
+        }
+
+
+        while(fast && fast.next && fast.next.next) {
+            slow =  slow.next;
+            fast = fast.next.next;
+        }
+
+        // create two new Linked list;
+
+        let next = slow.next;
+        slow.next = null;
+        // console.log("returning two halves")
+        // this.printL(head);
+        // this.printL(next);
+        return {firstHead: head, secondHead: next }
+
+
+    }
+
+    mergeLL(left, right) {
+     let mergedList = new LinkedList();
+     let lptr = left;
+     let rptr = right;
+    //  console.log("merging left and right halves")
+    //  this.printL(lptr);
+    //  this.printL(rptr)
+     while(lptr && rptr) {
+        if(lptr.val >  rptr.val) {
+            mergedList.insert(rptr.val)
+             rptr = rptr.next
+        } else {
+            mergedList.insert(lptr.val);
+            lptr = lptr.next;
+        }
+
+     }
+
+     while(lptr) {
+        mergedList.insert(lptr.val);
+        lptr = lptr.next;
+     }
+
+     while(rptr) {
+        mergedList.insert(rptr.val);
+        rptr = rptr.next;
+     }
+    //  console.log("merging ")
+    //  mergedList.print()
+    //  console.log("\n")
+     return mergedList.head;
+    }
+
+    sortLL(head) {
+        if(!head || !head.next)
+            return head;
+        let {firstHead, secondHead} = this.findtwoHalves(head);
+        // console.log(firstHead,secondHead)
+        
+       firstHead = this.sortLL(firstHead);
+       secondHead = this.sortLL(secondHead);
+       return this.mergeLL(firstHead,secondHead);
+    }
+
+    sort() {
+        this.head = this.sortLL(this.head);
+    }
+
 }
 
 
 module.exports = LinkedList
 
 if (require.main === module) {
-let L = new LinkedList()
-L.insert(2)
-L.insert(4)
-L.print()
-let p = L.insert(5)
-L.print()
-L.insert(1)
-L.print()
-let n = L.insert(3)
-L.print()
-n.next = p;
-console.log("cycle present at",L.detectCycle().val)
-n.next = null
-console.log("cycle present at",L.detectCycle().val)
-L.print()
-console.log("Deleting last")
-L.deletelast();
-L.print();
-console.log("Deleting First")
-L.deletefirst();
-L.print();
-L.insertHead(-1);
-L.insertHead(-2);
-L.print();
-L.deleteLinkedList();
-L.print();
+    let L = new LinkedList()
+    L.insert(2)
+    L.insert(4)
+    // L.print()
+    L.insert(5)
+    // L.print()
+    L.insert(10)
+    // L.print()
+    // let n = L.insert(30)
+    // L.print()
+    // n.next = p;
+    // console.log("cycle present at", L.detectCycle().val)
+    // n.next = null
+    // console.log("cycle present at", L.detectCycle().val)
+    // L.print()
+    // console.log("Deleting last")
+    // L.deletelast();
+    // L.print();
+    // console.log("Deleting First")
+    // L.deletefirst();
+    // L.print();
+    // L.insertHead(-1);
+    // L.insertHead(-2);
+    // L.print();
+    //L.deleteLinkedList();
+    L.insert(7)
+    // L.print();
+    L.insert(-60)
+    L.insert(-1)
+    L.print();
+    L.sort();
+    L.print();
+
 }
